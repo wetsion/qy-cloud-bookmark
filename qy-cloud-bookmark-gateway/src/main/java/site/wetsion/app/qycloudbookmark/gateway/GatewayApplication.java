3ca2c7440bd1153a5adbf245bdb961242d7d080e
Wetsion <13110018525@163.com>
2020-03-16 00:07:32 +0800

    [init] 初始化，增加网关

---
+++ qy-cloud-bookmark-gateway/src/main/java/site/wetsion/app/qycloudbookmark/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package site.wetsion.app.qycloudbookmark.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *
+ * Created by wetsion on 2020/3/15.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
